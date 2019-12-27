664eedfc1eb0f743d1ad2e7bb8091965117a6045
pvbergen <ph.vonbergen@iqual.ch>
2019-12-27 14:55:41 +0100

    Effects on public rendering, formatting, comments

--- src/Plugin/pagedesigner/Handler/Effects.php
+++ src/Plugin/pagedesigner/Handler/Effects.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace Drupal\pagedesigner_effects\Plugin\pagedesigner\Handler;
 
 use Drupal\pagedesigner\Entity\Element;
 use Drupal\pagedesigner\Plugin\CompoundHandlerBase;
 
 /**
+ * Add effects functionality to "row" and "component" patterns.
+ *
  * @PagedesignerHandler(
  *   id = "effects",
  *   name = @Translation("Effects handler"),
@@ -15,69 +18,78 @@ use Drupal\pagedesigner\Plugin\CompoundHandlerBase;
  *   weight = 50
  * )
  */
-class Effects extends CompoundHandlerBase
-{
+class Effects extends CompoundHandlerBase {
 
-    /**
-     * {@inheritDoc}
-     */
-    public function collectAttachments(&$attachments)
-    {
-        if (\Drupal::currentUser()->hasPermission('pd_effect_use')) {
-            $config = \Drupal::config('pagedesigner_effects.settings');
-            $attachments['drupalSettings']['pagedesigner_effects']['effects'] = $config->get('enable_effects');
-            $attachments['library'][] = 'pagedesigner_effects/pagedesigner';
-        }
+  /**
+   * {@inheritDoc}
+   */
+  public function collectAttachments(&$attachments) {
+    if (\Drupal::currentUser()->hasPermission('pd_effect_use')) {
+      $config = \Drupal::config('pagedesigner_effects.settings');
+      $attachments['drupalSettings']['pagedesigner_effects']['effects'] = $config->get('enable_effects');
+      $attachments['library'][] = 'pagedesigner_effects/pagedesigner';
     }
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function serialize(Element $entity, &$result = [])
-    {
-        $result['effects'] = json_decode($entity->field_effects->value, true);
+  /**
+   * {@inheritDoc}
+   */
+  public function serialize(Element $entity, &$result = []) {
+    $result['effects'] = json_decode($entity->field_effects->value, TRUE);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function get(Element $entity, &$result = '') {
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function patch(Element $entity, $data) {
+    if (!empty($data['effects'])) {
+      $entity->field_effects->value = json_encode($data['effects']);
+      $entity->save();
     }
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function get(Element $entity, &$result = '')
-    {
+  /**
+   * {@inheritDoc}
+   */
+  public function renderForPublic(Element $entity, &$build = []) {
+    $this->addEffects($entity, $build);
+  }
 
+  /**
+   * {@inheritDoc}
+   */
+  public function render(Element $entity, &$build = []) {
+    $this->addEffects($entity, $build);
+  }
+
+  /**
+   * Add the user generated effects of the entity to drupalSettings.
+   */
+  protected function addEffects(Element $entity, &$build = []) {
+    if (!$entity->hasField('field_effects') || $entity->field_effects->isEmpty()) {
+      return;
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function patch(Element $entity, $data)
-    {
-        if (!empty($data['effects'])) {
-            $entity->field_effects->value = json_encode($data['effects']);
-            $entity->save();
-        }
+    if (empty($build['#attached'])) {
+      $build['#attached'] = [];
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function render(Element $entity, &$build = [])
-    {
-        if ($entity->field_effects->isEmpty()) {
-            return;
-        }
-        if (empty($build['#attached'])) {
-            $build['#attached'] = [];
-        }
-        if (empty($build['#attached']['drupalSettings'])) {
-            $build['#attached']['drupalSettings'] = [];
-        }
-        if (empty($build['#attached']['drupalSettings']['pagedesigner'])) {
-            $build['#attached']['drupalSettings']['pagedesigner'] = [];
-        }
-        if (empty($build['#attached']['drupalSettings']['pagedesigner']['effects'])) {
-            $build['#attached']['drupalSettings']['pagedesigner']['effects'] = [];
-        }
-        $build['#attached']['drupalSettings']['pagedesigner']['effects']['#pd-cp-' . $entity->id()] = json_decode($entity->field_effects->value);
-
+    if (empty($build['#attached']['drupalSettings'])) {
+      $build['#attached']['drupalSettings'] = [];
     }
+    if (empty($build['#attached']['drupalSettings']['pagedesigner'])) {
+      $build['#attached']['drupalSettings']['pagedesigner'] = [];
+    }
+    if (empty($build['#attached']['drupalSettings']['pagedesigner']['effects'])) {
+      $build['#attached']['drupalSettings']['pagedesigner']['effects'] = [];
+    }
+    $build['#attached']['drupalSettings']['pagedesigner']['effects']['#pd-cp-' . $entity->id()] = json_decode($entity->field_effects->value);
+
+  }
+
 }
